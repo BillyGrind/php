0efb12a6bb0ffde91b13e3aee8105385491f475f
BillyGrind <denayeranthony623@gmail.com>
2023-06-02 11:38:14 +0200

    unicorn done

--- unicorn.php
+++ unicorn.php
@@ -26,20 +26,33 @@ if(isset($_GET['submit'])){
 
     $type = $_GET['type'];
 
+    $humain ="";
+    $cat ="";
+    $unicorn ="";
 
-    switch($type){
-        case 'humain':
-            echo'<iframe src="https://giphy.com/embed/aILCLOWVzT0Sk" width="320" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/animation-art-3d-aILCLOWVzT0Sk">via GIPHY</a></p>';
-            break;
-        case 'cat':
-            echo'<div style="width:480px"><iframe allow="fullscreen" frameBorder="0" height="270" src="https://giphy.com/embed/IsDjNQPc4weWPEwhWm/video" width="480"></iframe></div>';
-            break;
-        case 'unicorn':
-            echo'<div style="width:480px"><iframe allow="fullscreen" frameBorder="0" height="270" src="https://giphy.com/embed/Ze44chcCSUpyVN1gmQ/video" width="480"></iframe></div>';
-            break;
-        default:
-            echo "ERROOOOOOR"                ;
-    }
+    $is_humain= ($type =='humain') ? $humain ='<iframe src="https://giphy.com/embed/aILCLOWVzT0Sk" width="320" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/animation-art-3d-aILCLOWVzT0Sk">via GIPHY</a></p>':false;
+    
+    $is_cat= ($type =='cat') ?  $cat='<div style="width:480px"><iframe allow="fullscreen" frameBorder="0" height="270" src="https://giphy.com/embed/IsDjNQPc4weWPEwhWm/video" width="480"></iframe></div>':false;
+
+     $is_unicorn= ($type =='unicorn') ? $unicorn='<div style="width:480px"><iframe allow="fullscreen" frameBorder="0" height="270" src="https://giphy.com/embed/Ze44chcCSUpyVN1gmQ/video" width="480"></iframe></div>':false;
+
+        echo $humain;
+        echo $cat;
+        echo $unicorn;
+
+    // switch($type){
+    //     case 'humain':
+    //         echo'<iframe src="https://giphy.com/embed/aILCLOWVzT0Sk" width="320" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/animation-art-3d-aILCLOWVzT0Sk">via GIPHY</a></p>';
+    //         break;
+    //     case 'cat':
+    //         echo'<div style="width:480px"><iframe allow="fullscreen" frameBorder="0" height="270" src="https://giphy.com/embed/IsDjNQPc4weWPEwhWm/video" width="480"></iframe></div>';
+    //         break;
+    //     case 'unicorn':
+    //         echo'<div style="width:480px"><iframe allow="fullscreen" frameBorder="0" height="270" src="https://giphy.com/embed/Ze44chcCSUpyVN1gmQ/video" width="480"></iframe></div>';
+    //         break;
+    //     default:
+    //         echo "ERROOOOOOR";
+    // }
 }
 ?>
 
